8f955d5c42526def317b6b5f7a2701cc588ba6d6
Luis Gutierrez <111073914+LuisGutierrerez18@users.noreply.github.com>
2023-05-26 20:20:41 -0600

    Add files via upload

---
+++ team.h
@@ -0,0 +1,71 @@
+/*
+ * Fantasy Team Creator Proyect forward.h class
+ * Luis Enrique Gutierrez 
+ * 
+ * For: Object Oriented Programming 2
+ * Class will define an specific object type. In this file: the complete team
+ * Will include an array in which every player would be added to.
+ * Also the budget would be managed in this section.
+*/
+
+#include<iostream> // prints 
+#include <string>
+using namespace std;
+
+#include "player.h"
+
+class team
+{
+    private:
+        double budget = 100.0; // Team Budget assigned to 100 by default
+        
+    protected:
+        string type; // type of player
+
+    public:
+        string team[16]; // list of all the players selected in the team
+        double price; 
+
+        /* Methods */
+        
+        /*
+         * Will receive the price of each player and subtract it from the club's budget
+        */
+        void adjust_budget()
+        {
+            double max_price = 10.0;
+            /* Test */
+            cout << "Enter the price of the player: ";
+            cin >> price;
+
+            if(price <= max_price)
+            {
+                budget = budget - price;
+                cout<< "Budget Left:" << budget;
+            }
+                
+        }
+
+        /*
+         * Method will add all the players to the principal team list
+        */
+        void push_player()
+        {
+            int i = 0;
+            string name;
+
+            /* Test */
+            while (i <= 16)
+            {
+                cout<< "Enter the number of the player: ";
+                cin>> name;
+
+                
+                
+                i++;
+            }
+
+            cout<<team;
+        }
+
+};
