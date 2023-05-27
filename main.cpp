84c81214fa5b8386381094bedb29d8322d4a0125
Luis Gutierrez <111073914+LuisGutierrerez18@users.noreply.github.com>
2023-05-26 20:21:56 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Fantasy Team Creator Proyect main class
+ * Luis Enrique Gutierrez 
+ * 
+ * For: Object Oriented Programming 2
+ * Proyect designed as an interactive tool in which the user creates his own soccer team. 
+ * Includes many features as the type of positions, managers and club budget. 
+*/
+
+#include<iostream> // print
+
+#include "player.h"
+#include "goalkeeper.h"
+#include "defender.h"
+# include "midfielder.h"
+#include "forward.h"
+#include "manager.h"
+#include "team.h" 
