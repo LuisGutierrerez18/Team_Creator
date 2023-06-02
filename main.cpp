db401fab5e488ba9dc6a6cf1cffb59dec878d9a4
Luis Gutierrez <111073914+LuisGutierrerez18@users.noreply.github.com>
2023-06-02 11:33:25 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,6 +13,43 @@
 #include "goalkeeper.h"
 #include "defender.h"
 # include "midfielder.h"
-#include "forward.h"
+#include "attacker.h"
 #include "manager.h"
 #include "team.h" 
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to the Team Creator Fantasy \n";
+    cout << "\n"
+    cout << "Rules are Simple: You Enter The Name We Add it to the Team List \n";
+    
+    // Creating our player objects 
+
+    // goalkeeper gk("Ter Stegen", 88, 1992, 182, 100);
+    // defender def("Araujo", 83, 1999, 40, 2, 30);
+    // midfielder mid("de Jong", 87, 1997, 21, 88);
+    // attacker fwrd("Messi", 94, 1988, 95, 50, 120);
+    // manager mngr("Manchester City", "Guardiola");
+
+    //string myTeam[]; // Creates the team object in which evrything would be stored
+    
+    // Adding names to the list
+    // myTeam.add(gk);
+    // myTeam.add(def);
+    // myTeam.add(mid);
+    // myTeam.add(fwrd);
+    // myTeam.add(mngr);
+
+    // string input; //Variable for input value
+    // cout<< "Enter Player's Name";
+    // cin>> input;
+
+    // myTeam.search_Player(input);
+
+    // myTeam.push_player(input);
+
+    
+
+}
