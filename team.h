8979c92be537cc94e36cf36bce00f20970ccbb3e
Luis Gutierrez <111073914+LuisGutierrerez18@users.noreply.github.com>
2023-06-02 11:30:57 -0600

    Add files via upload

--- team.h
+++ team.h
@@ -10,30 +10,38 @@
 
 #include<iostream> // prints 
 #include <string>
-using namespace std;
-
 #include "player.h"
+using namespace std;
 
 class team
 {
     private:
         double budget = 100.0; // Team Budget assigned to 100 by default
-        
+        int counter =0;
+        static const int teamSize = 16;
+
+        // Siguiendo ejemplo del profe 
+        Team *teamList[teamSize]; // Pointers --> array 
     protected:
         string type; // type of player
 
     public:
-        string team[16]; // list of all the players selected in the team
+        //string team[16]; // list of all the players selected in the team
         double price; 
+
+        /* Team Constructor */
+        team(string t)
+        {
+            
+        }
 
         /* Methods */
         
         /*
          * Will receive the price of each player and subtract it from the club's budget
         */
-        void adjust_budget()
+        void adjust_budget(double max_price = 10.0) // This max_price is the maximum price of any individual player 
         {
-            double max_price = 10.0;
             /* Test */
             cout << "Enter the price of the player: ";
             cin >> price;
@@ -65,7 +73,7 @@ class team
                 i++;
             }
 
-            cout<<team;
+            cout<<teamC;
         }
 
 };
