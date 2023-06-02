24e87fcd46f678626831136a5ff2573c5a2975cc
Luis Gutierrez <111073914+LuisGutierrerez18@users.noreply.github.com>
2023-06-02 11:31:38 -0600

    Add files via upload

--- player.h
+++ player.h
@@ -12,8 +12,7 @@
 using namespace std;
 
 
-class player
-{
+class player{
     protected: // declaration of Protected variables 
         string name; // player's name
         int rating; // player's rating
@@ -22,12 +21,21 @@ class player
         int birthday; // player's birthday
     
     public:
+
+        /* Contructor */
+        /*player(string n, int rat, string count, string pos, int birth) {
+            name = n;
+            rating = rat; 
+            country = count; 
+            position = pos;
+            birthday = birth;
+        }*/
+
         /*
          * Public search_player method will search for a desired player
          * Generate a response if the player was found 
         */
-        void search_player()
-        {
+        void search_player(){
             /* List of available goalkeepers */
             string goalKeeperList[] = {"Ter Stegen", "Courtois", "Donnaruma", "Ederson", "Pope"}; // Array with the names of goalkeepers
             int length_GK_List = sizeof(goalKeeperList)/sizeof(goalKeeperList[0]); //Will return the length of goalkeer Array 
@@ -116,16 +124,14 @@ class player
         /*
          * Getter method: 'get_name' designed to get the name of the player   
         */
-        string get_name()
-        {
+        string get_name(){
             return name;
         }
 
         /*
          * Setter method: 'set_name' designed to update the name of the player
         */
-        void set_name(string newName)
-        {
+        void set_name(string newName){
             // Data type: string
             name = newName;
         }
@@ -133,16 +139,14 @@ class player
         /*
          * Getter method: 'get_rating' which will return the rating of the player
         */
-        int get_rating()
-        {
+        int get_rating(){
             return rating;
         }
 
         /*
          * Setter method 'set_rating' will update the rating value 
         */
-        void set_rating(int newRating)
-        {
+        void set_rating(int newRating){
             // Data type: int
             rating = newRating;
         }
@@ -150,16 +154,14 @@ class player
         /*
          * Getter Method: 'get_country' will return the country of origign of the player
         */
-        string get_country()
-        {
+        string get_country(){
             return country;
         }
 
         /*
          * Setter method 'set_country' will update the value of country  
         */
-        void set_country(string newCountry)
-        {
+        void set_country(string newCountry){
             // Data type: string
             country = newCountry;
         }
@@ -167,38 +169,33 @@ class player
         /*
          * Getter method 'get_position' will return the position of the player 
         */
-        string get_position()
-        {
+        string get_position(){
             return position;
         }
 
         /*
          * Setter method 'set_position' will update the value of position  
         */
-        void set_position(string newPos)
-        {
+        void set_position(string newPos){
             position = newPos;
         }
 
         /*
          * Getter method 'get_birthday' will return the player's year of birth 
         */
-        int get_birthday()
-        {
+        int get_birthday(){
             return birthday;
         }
 
         /*
          * Setter method 'set_position' will update the value of year of birth  
         */
-        void set_birthday(int newBirth)
-        {
+        void set_birthday(int newBirth) {
             birthday = newBirth;
         }
 
         // Returns the exact age of the player
-        void get_age()
-        {
+        void get_age(){
             int year; // Current year
             cout<< "Enter the current year";
             cin >> year;
@@ -215,6 +212,16 @@ class player
             }
 
             age = year - birthday; // Calculation to determine the age
+        }
+
+        /*
+         * This method would be used as polymorphism
+         * Would indicate the action a playing is making
+         * Example: If the player is a midfielder polymorphism would change the outcome of the method as: giving assists
+         * Makes the interaction more dynamic   
+        */
+        void playing(){
+            cout << "The Player is playing";
         }
     
 };
