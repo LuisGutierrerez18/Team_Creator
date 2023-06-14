53ece56d3efc08ad00cc18882a89897fe1cc1519
Luis Gutierrez <111073914+LuisGutierrerez18@users.noreply.github.com>
2023-06-14 09:42:44 -0600

    Beta Version 1.3

--- main.cpp
+++ main.cpp
@@ -62,7 +62,7 @@ int main()
     // Adjusts the budget of the team
     myTeam.adjust_budget();
     cout<<"|============|===============| \n";
-    cout<<"| Name | Rating | Year of Birth | Stat/Hegight | Cleansheet/shot |";
+    cout<<"| Name | Rating | Year of Birth | Stat/Hegight | Cleansheet/shot | \n";
     cout<<myTeam.to_string();
       
 
