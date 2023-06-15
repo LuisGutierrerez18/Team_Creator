14575f43566e551579206546c47269229fbf3727
Luis Gutierrez <111073914+LuisGutierrerez18@users.noreply.github.com>
2023-06-15 10:17:17 -0600

    Optimization Update

--- team.h
+++ team.h
@@ -38,12 +38,103 @@ class Team
         /* Methods */
 
         /*
+         * Public search_player method will search for a desired player
+         * Generate a response if the player was found
+        */
+        void search_player(){
+            /* List of available goalkeepers */
+            string name;
+            string goalKeeperList[] = {"Ter Stegen", "Courtois", "Donnaruma", "Ederson", "Pope"}; // Array with the names of goalkeepers
+            int length_GK_List = sizeof(goalKeeperList)/sizeof(goalKeeperList[0]); //Will return the length of goalkeer Array
+            // And will be used for the for loop transversing
+
+            /* List of available defenders */
+            string defenderList[] = {"Van Dijk", "Ramos", "Trippier", "Balde", "Maguire", "Araujo", "Hakimi", "Alaba", "Christensen", "Stones"};
+            int length_def_List = sizeof(defenderList)/sizeof(defenderList[0]); //Will return the length of defender Array
+            // And will be used for the for loop transversing
+
+            /* List of avaiable midfielders */
+            string midfielderList[] = {"de Jong", "De Bruyne", "Bellingham", "Pedri", "Gavi", "Kroos", "Modric", "Saka", "Kimmich", "Gundogan"};
+            int length_mid_List = sizeof(defenderList)/sizeof(defenderList[0]); //Will return the length of midfielder Array
+            // And will be used for the for loop transversing
+
+            // List of avaible attacker
+            string attackerList[] = {"Messi", "Demeble", "Lewandowski", "Haaland", "Mbappe", "Salah", "Neymar", "Lukaku","Ronaldo"};
+            int length_frwd_List = sizeof(defenderList)/sizeof(defenderList[0]); //Will return the length of attacker Array
+            // And will be used for the for loop transversing
+
+            cout<<"Be advised to use player's last name \n"; // Use only last name of the player
+            cout<<"\nEnter the player's name: ";
+            cin >> name;
+
+            bool playerFound = false; // the default return value for searching a player will be false until player is found
+
+            // For loop which will traverse through array looking for player
+            for(int i = 0; i < length_GK_List; i++)
+            {
+                if(name == goalKeeperList[i]) // Once traversed will look at each index looking for the name
+                {
+                    cout<< name << " Was Found and Available \n";
+                    playerFound = true;
+                    break;
+                }
+            }
+
+            // if not found will repeat process and search on the defenders array
+            if(!playerFound){
+                for(int i = 0; i < length_def_List; i++)
+                {
+                    if(name == defenderList[i]) // Once traversed will look at each index looking for the name
+                    {
+                        cout<< name << " Was Found and Available\n";
+                        cout<<"\nHas been succesfully added to your team";
+                        playerFound = true;
+                        break;
+                    }
+                }
+            }
+
+            // if not found will repeat process and search on the midfielders array
+            if(!playerFound){
+                for(int i = 0; i < length_mid_List; i++)
+                {
+                    if(name == midfielderList[i]) // Once traversed will look at each index looking for the name
+                    {
+                        cout<< name << " Was Found and Available \n";
+                        cout<<"Has been succesfully added to your team";
+                        playerFound = true;
+                        break;
+                    }
+                }
+            }
+
+            // if not found will repeat process and search on the attackers array
+            if(!playerFound){
+                for(int i = 0; i < length_frwd_List; i++)
+                {
+                    if(name == attackerList[i]) // Once traversed will look at each index looking for the name
+                    {
+                        cout<< name << " Was Found and Available \n";
+                        cout<<"Has been succesfully added to your team";
+                        playerFound = true;
+                        break;
+                    }
+                }
+            }
+
+            // Finally
+            // if the player is not found on any of the arrays then it will display a message saying the player was not found
+            if(!playerFound){
+                cout<< "Player Was NOT Found \n";
+            }
+        }
+        /*
          * Will receive the price of each player and subtract it from the club's budget
         */
         void adjust_budget(double max_price = 10.0) { // This max_price is the maximum price of any individual player{
             /* Test */
             budget = 100.0;
-            cout << "Enter the price of the player: ";
+            cout << "\nEnter the price of the player: ";
             cin >> price;
 
             if(price <= max_price)
@@ -60,20 +151,18 @@ class Team
         */
         void push_player(){
             int i = 0;
-            string name;
+            //string name;
 
             /* Test */
-            while (i <= 16)
+            while (i <= 4)
             {
-                cout<< "Enter the number of the player: ";
-                cin>> name;
-
-
+                cout<<"\n";
+                search_player();
 
                 i++;
             }
 
-            cout<<name;
+            //cout<<name;
         }
 
         /*
